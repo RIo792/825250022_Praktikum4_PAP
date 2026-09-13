dd60e03761756322c820d9614baf9b0255e2916b
Rio792 <rio24r0307@gmail.com>
2026-09-13 08:19:33 +0700

    second

--- script.js
+++ script.js
@@ -1,51 +1,21 @@
-// Fungsi untuk membuat persegi panjang ID
-function buatID() {
+function tampilkanID() {
+    // Ambil tempat kartu
+    let data = document.getElementById("data");
 
-    // Mengambil tempat untuk menampilkan ID
-    const areaID = document.getElementById("areaID");
-
-    // Mengecek apakah ID sudah dibuat
-    if (document.getElementById("kartuID")) {
-        alert("ID sudah dibuat!");
-        return;
-    }
-
-    // Membuat elemen div baru
-    const kartu = document.createElement("div");
-
-    // Memberikan ID pada elemen yang dibuat
-    kartu.id = "kartuID";
-
-    // Memberikan class CSS pada elemen
-    kartu.className = "kartu-id";
-
-    // Mengisi isi persegi panjang dengan Nama dan NIM
-    kartu.innerHTML = `
-        <h2>IDENTITAS MAHASISWA</h2>
-        <p><strong>Nama:</strong> Rio</p>
-        <p><strong>NIM:</strong> 825250022</p>
+    // Tampilkan kartu
+    data.innerHTML = `
+        <div class="kartu">
+            <h2>Data Mahasiswa</h2>
+            <p>Nama: Rio</p>
+            <p>NIM: 8252520022 </p>
+        </div>
     `;
-
-    // Menampilkan persegi panjang ke halaman
-    areaID.appendChild(kartu);
 }
 
-
-// Fungsi untuk menghapus persegi panjang ID
 function hapusID() {
+    // Ambil kartu
+    let data = document.getElementById("data");
 
-    // Mengambil elemen ID yang sudah dibuat
-    const kartu = document.getElementById("kartuID");
-
-    // Mengecek apakah ID tersedia
-    if (kartu) {
-
-        // Menghapus elemen ID
-        kartu.remove();
-
-    } else {
-
-        // Menampilkan pesan jika ID belum dibuat
-        alert("ID belum dibuat!");
-    }
+    // Hapus kartu
+    data.innerHTML = "";
 }
